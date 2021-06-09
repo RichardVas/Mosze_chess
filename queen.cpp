1dfc66428541d76384b39d3fa9765d040153f7c1
Varga Máté <matevarga96@gmail.com>
2021-06-09 22:05:53 +0200

    Add queen movement

--- queen.cpp
+++ queen.cpp
@@ -3,7 +3,7 @@
 bool Queen::checkMove(int dest_x, int dest_y) {
     /*a királynő a saját oszlopában, sorában és az átlók mentén léphet tetszőlegesen, */
     bool res=false;
-    if(){
+    if((this->getX-dest_x!=0)&&(this->getY-dest_y==0) || (this->getY-dest_y!=0)&&(this->getX-dest_x==0)||abs(this->getX-dest_x)==abs(this->getY-dest_y)){
         res=true;
     }
     else{
