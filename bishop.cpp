58b964be800afa0558281e78644a7d58efcb3862
Varga Máté <matevarga96@gmail.com>
2021-06-09 21:06:56 +0200

    Add bishop movement

--- bishop.cpp
+++ bishop.cpp
@@ -3,8 +3,9 @@
 bool Bishop::checkMove(int dest_x, int dest_y) {
     /*a futó léphet bármerre átlósan*/
     bool res=true;
-    if(){
+    if(abs(this->getX-dest_x)!=abs(this->getY-dest_y)){
         res=false;
+        std::cout << "rossz lepes\n";
     }
 return res;
 }
