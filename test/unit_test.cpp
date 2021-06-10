19c5d8e4532cd6552cd848b79b0bba17c0ac39fd
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-10 22:21:34 +0200

    Add files via upload

---
+++ test/unit_test.cpp
@@ -0,0 +1,15 @@
+#include "../JSON.h"
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <utility>
+
+
+
+
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
