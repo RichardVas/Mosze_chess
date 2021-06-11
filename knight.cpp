25e50646db2f7957023b38ce26a0904d77a503cf
Varga Máté <matevarga96@gmail.com>
2021-06-11 20:59:07 +0200

    Fix knight movement

--- knight.cpp
+++ knight.cpp
@@ -3,7 +3,8 @@
 bool Knight::checkMove(int dest_x, int dest_y) {
     /*a huszárnak egy lépéssel mindig 2 mezőt kell mennie egy irányba, és egyet a másik irányba tetszőleges sorrendben, és átléphet más figurák felett*/
     bool res=false;
-    if(((this->getX+1)||(this->getX-1)==dest_x && (this->getY+2)||(this->getY-2)==dest_y) || ((this->getX+2)||(this->getX-2)==dest_x && (this->getY+1)||(this->getY-1)==dest_y)){
+    int dist=abs((this->getX-dest_x)+(this->getY-dest_y));
+    if(dist==3 && this->getX-dest_x!=0 && this->getY-dest_y!=0){
         res=true;
     }
     else{
