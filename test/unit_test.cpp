d32be108f4f05a734f14b25fbfd1434008e188f0
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2021-06-12 15:14:49 +0200

    Update unit_test.cpp

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,10 +1,19 @@
 #include "../JSON.h"
+#include "King.h"
 #include <gtest/gtest.h>
 
 #include <fstream>
 #include <utility>
 
-
+TEST(King_piece,King_exists)
+{
+	Piece *kiraly = new King(10,10);
+	int test_x = kiraly->getX();
+	int exp_x = 10;
+	
+	delete kiraly;
+	ASSERT_EQ(test_x, exp_x);
+}
 
 
 int main(int argc, char** argv)
